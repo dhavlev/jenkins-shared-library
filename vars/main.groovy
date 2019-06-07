c0a35a6715c21781f63b2e0daa6336e81687f439
Dhavle <visharad.dhavle@here.com>
2019-06-07 13:12:56 +0530

    beta: optimising code to add facade

---
+++ vars/main.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/groovy
+
+import org.my.jenkins.*
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object    
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+
+    ci{
+        ci = config.ci
+    }
+
+    //cd{
+    //    cd = config.cdConfig
+    //}
+}
