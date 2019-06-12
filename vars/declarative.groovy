07ec735805ef756d8af4861f998f1a0bcccc2467
Dhavle <visharad.dhavle@here.com>
2019-06-12 15:11:33 +0530

    declarative: UI still shows straigth pipeline+1

--- vars/declarative.groovy
+++ vars/declarative.groovy
@@ -40,27 +40,34 @@ pipeline {
                 FOR_SEQUENTIAL = "some-value"
             }
             stages {
-                container('maven') {
                     stage('In Sequential 1') {
                         steps {
-                            echo "In Sequential 1"
+                            container('maven') {
+                                echo "In Sequential 1"
+                            }
                         }
                     }
                     stage('In Sequential 2') {
                         steps {
-                            echo "In Sequential 2"
+                            container('maven') {
+                                echo "In Sequential 2"
+                            }
                         }
                     }
                     stage('Parallel In Sequential') {
                         parallel {
                             stage('In Parallel 1') {
                                 steps {
-                                    echo "In Parallel 1"
+                                    container('maven') {
+                                        echo "In Parallel 1"
+                                    }
                                 }
                             }
                             stage('In Parallel 2') {
                                 steps {
-                                    echo "In Parallel 2"
+                                    container('maven') {
+                                        echo "In Parallel 2"
+                                    }
                                 }
                             }
                         }
@@ -69,5 +76,4 @@ pipeline {
             }
         }
     }
-}
 }
