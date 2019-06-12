93e9980a6fe3215096140fa24bc238d31b9a1798
Dhavle <visharad.dhavle@here.com>
2019-06-12 15:09:21 +0530

    declarative: UI still shows straigth pipeline

--- vars/declarative.groovy
+++ vars/declarative.groovy
@@ -11,54 +11,56 @@ def call(body) {
     String podConfig = libraryResource "podConfig.yaml"
     def podLabel = "ci-${UUID.randomUUID().toString()}"
 
-    pipeline {
-        agent none
-        stages {
-            stage('Stage-Sq-1') {
-                agent {
-                    kubernetes {
-                        label podLabel
-                        defaultContainer 'jnlp'
-                        yaml podConfig
-                    }
-                }
-                steps {
-                    container('maven') {
-                        sh 'mvn -version'
-                    }
+pipeline {
+    agent none
+    stages {
+        stage('Non-Sequential Stage') {
+            agent {
+                kubernetes {
+                    label podLabel
+                    defaultContainer 'jnlp'
+                    yaml podConfig
                 }
             }
-
-            stage('Stage-A') {
-                agent {
-                    kubernetes {
-                        label podLabel
-                        defaultContainer 'jnlp'
-                        yaml podConfig
-                    }
+            steps {
+                container('maven') {
+                    echo "On Non-Sequential Stage"
                 }
-                
-                stages {
-                    stage("Stage-B1"){
-                        steps{
-                            echo "Stage B1"
+            }
+        }
+        stage('Sequential') {
+            agent {
+                kubernetes {
+                    label podLabel
+                    defaultContainer 'jnlp'
+                    yaml podConfig
+                }
+            }
+            environment {
+                FOR_SEQUENTIAL = "some-value"
+            }
+            stages {
+                container('maven') {
+                    stage('In Sequential 1') {
+                        steps {
+                            echo "In Sequential 1"
                         }
                     }
-                    stage("Stage-B2"){
-                        steps{
-                            echo "Stage B2"
+                    stage('In Sequential 2') {
+                        steps {
+                            echo "In Sequential 2"
                         }
                     }
-                    stage("Parallel Stage"){
-                        parallel{
-                            stage("Stage-C1"){
-                                steps{
-                                    echo "Paraller Stage C1"
+                    stage('Parallel In Sequential') {
+                        parallel {
+                            stage('In Parallel 1') {
+                                steps {
+                                    echo "In Parallel 1"
                                 }
                             }
-                            stage("Stage-C2"){
-                                steps{
-                                    echo "Paraller Stage C2"
+                            stage('In Parallel 2') {
+                                steps {
+                                    echo "In Parallel 2"
                                 }
                             }
                         }
@@ -67,4 +69,5 @@ def call(body) {
             }
         }
     }
+}
 }
