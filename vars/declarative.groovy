38519e95fb890109796e62bcc6c8fe846d022139
Dhavle <visharad.dhavle@here.com>
2019-06-12 14:43:33 +0530

    declarative: sequential and non sequential steps added

--- vars/declarative.groovy
+++ vars/declarative.groovy
@@ -12,7 +12,7 @@ def call(body) {
     def podLabel = "ci-${UUID.randomUUID().toString()}"
 
     pipeline {
-        agent {
+        agent none {
             kubernetes {
                 label podLabel
                 defaultContainer 'jnlp'
@@ -20,10 +20,53 @@ def call(body) {
             }
         }
         stages {
-            stage('Run maven') {
+            stage('Stage-Sq-1') {
                 steps {
+                    agent {
+                        kubernetes {
+                        label podLabel
+                        defaultContainer 'jnlp'
+                        yaml podConfig
+                    }
+
                     container('maven') {
                         sh 'mvn -version'
+                    }
+                }
+            }
+
+            stage('Stage-A') {
+                agent {
+                    kubernetes {
+                    label podLabel
+                    defaultContainer 'jnlp'
+                    yaml podConfig
+                }
+                
+                stages{
+                    stage("Stage-B1"){
+                        step{
+                            echo "Stage B1"
+                        }
+                    }
+                    stage("Stage-B2"){
+                        step{
+                            echo "Stage B2"
+                        }
+                    }
+                    stage("Parallel Stage"){
+                        parallel{
+                            stage("Stage-C1"){
+                                step{
+                                    echo "Paraller Stage C1"
+                                }
+                            }
+                            stage("Stage-C2"){
+                                step{
+                                    echo "Paraller Stage C2"
+                                }
+                            }
+                        }
                     }
                 }
             }
