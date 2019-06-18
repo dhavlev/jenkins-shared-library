ec21c8da0c59511d8ae92f8f3cc757c0726a5190
Dhavle <visharad.dhavle@here.com>
2019-06-18 19:40:43 +0530

    voting-app: repository and tag moved to appropriate place

--- src/org/my/jenkins/helm.groovy
+++ src/org/my/jenkins/helm.groovy
@@ -2,9 +2,6 @@ package org.my.jenkins
 
 void install(ciConfig, cdConfig) 
 {
-    def repository = "${ciConfig.dockerRegistry}/${ciConfig.repoName}"
-    def tag = "${ciConfig.version}.${env.BUILD_ID}"
-
     git(
         branch: cdConfig.chart.branch, 
         url: cdConfig.chart.repo
@@ -19,6 +16,9 @@ void install(ciConfig, cdConfig)
     }
     else
     {
+        def repository = "${ciConfig.dockerRegistry}/${ciConfig.repoName}"
+        def tag = "${ciConfig.version}.${env.BUILD_ID}"
+        
         createNamspace(cdConfig.repoName)
         sh(
             "helm upgrade --install ${ciConfig.repoName} ${ciConfig.repoName} \
